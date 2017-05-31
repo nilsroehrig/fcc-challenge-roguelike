fca6d7ffede953bd762b0153a61f4abcac0208a3
Nils Roehrig <der.user.00@gmail.com>
2017-05-31 20:46:17 +0200

    moved state management to own module

---
+++ src/game/StateManager.js
@@ -0,0 +1,157 @@
+import {getWeapon} from './Weapons.js';
+import FieldTypes from './FieldTypes';
+import DungeonGenerator from './DungeonGenerator';
+import EnemyGenerator from './EnemyGenerator';
+import {randomIntBetween} from '../utils/MathUtils';
+
+export function createInitialState() {
+    let dungeon = DungeonGenerator();
+    let flatMap = dungeon.map.reduce((acc, row) => {
+        acc.push.apply(acc, row);
+        return acc;
+    }, []);
+
+    let {x, y} = flatMap.filter(item => item.type === FieldTypes.Types.player)[0];
+
+    let weapon = getWeapon(0);
+
+    return {
+        dungeon: DungeonGenerator(),
+        player: {
+            health: 100,
+            attack: 7,
+            weapon: getWeapon(0),
+            level: 1,
+            position: {x, y},
+            exp: 0
+        }
+    };
+}
+
+function removeFromMap(field, map) {
+    return map.slice().map(row => {
+        let idx = row.indexOf(field);
+        if (idx === -1) {
+            return row;
+        }
+
+        let newRow = row.slice();
+        newRow[idx] = Object.assign({}, newRow[idx], {type: FieldTypes.Types.earth});
+    });
+}
+
+function levelUp(player) {
+    let newPlayer = Object.assign({}, player);
+    newPlayer.level += 1;
+    newPlayer.attack += randomIntBetween(8, 12) * newPlayer.level;
+    return newPlayer;
+}
+
+function fight(state, fieldWithEnemy) {
+    let player = Object.assign({}, state.player);
+    let enemy = Object.assign({}, (fieldWithEnemy.enemy) ? fieldWithEnemy.enemy: EnemyGenerator(state.dungeonLevel));
+    let newMap = state.dungeon.map;
+
+    player.health -= enemy.attack;
+    enemy.health -= player.attack + player.weapon.attack;
+
+    if (enemy.health < 0) {
+        player.exp += enemy.exp;
+        player.position.x = fieldWithEnemy.x;
+        player.position.y = fieldWithEnemy.y;
+        newMap = removeFromMap(fieldWithEnemy, state.dungeon.map);
+        enemy = null;
+    }
+
+    if (player.exp > (player.level * 1000)) {
+        player = levelUp(player);
+    }
+
+    return Object.assign({}, state, player, {dungeon: Object.assign({}, state.dungeon, {map: newMap})});
+}
+
+function takeAction(position, state) {
+    let field = state.map[position.y][position.x];
+    let newState;
+
+    switch(field.type) {
+        case FieldTypes.rock:
+            newState = state;
+            break;
+
+        case FieldTypes.earth:
+            newState = Object.assign(state, {playerPosition:{x: field.x, y: field.y}});
+            break;
+
+        case FieldTypes.enemy:
+            newState = fight(state, field);
+            break;
+    }
+
+    return newState;
+}
+
+function moveUp(state) {
+    let newField = {
+        x: state.playerPosition.x,
+        y: state.playerPosition.y - 1
+    };
+
+    return takeAction(newField, state);
+}
+
+function moveRight(state) {
+    let newField = {
+        x: state.playerPosition.x + 1,
+        y: state.playerPosition.y
+    };
+
+    return takeAction(newField, state);
+}
+
+function moveDown(state) {
+    let newField = {
+        x: state.playerPosition.x,
+        y: state.playerPosition.y + 1
+    };
+
+    return takeAction(newField, state);
+}
+
+function moveLeft(state) {
+    let newField = {
+        x: state.playerPosition.x - 1,
+        y: state.playerPosition.y
+    };
+
+    return takeAction(newField, state);
+}
+
+export function getReducer(initialState) {
+    return function(state, action) {
+        if (state === undefined) {
+            return initialState;
+        }
+
+        let newState = state;
+
+        switch(action) {
+            case 'MOVE_LEFT':
+            newState = moveLeft(state);
+            break;
+            case 'MOVE_RIGHT':
+            newState = moveRight(state);
+            break;
+            case 'MOVE_UP':
+            newState = moveUp(state);
+            break;
+            case 'MOVE_DOWN':
+            newState = moveDown(state);
+            break;
+        }
+
+        return newState;
+    };
+}
+
+export default {createInitialState, getReducer};
