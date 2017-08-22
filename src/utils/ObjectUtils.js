0404bbfbef1b44a4bff996b6721c31b59b450c10
Nils Roehrig <der.user.00@gmail.com>
2017-08-23 00:03:19 +0200

    remove superfluous export, update gitignore

--- src/utils/ObjectUtils.js
+++ src/utils/ObjectUtils.js
@@ -1,8 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 export function switchKeysAndValues(obj) {
     return Object.keys(obj).reduce((acc, key) => {
         acc[obj[key]] = key;
         return acc;
     }, {});
 }
-
-// export default {switchKeysAndValues}
